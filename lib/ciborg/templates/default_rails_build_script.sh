1098ef1ae560e23ced2daaaf6e81dc3c725cad57
Ken Mayer <ken@bitwrangler.com>
2013-11-07 15:02:45 -0800

    Update default_rails_build_script.sh

--- lib/ciborg/templates/default_rails_build_script.sh
+++ lib/ciborg/templates/default_rails_build_script.sh
@@ -3,11 +3,22 @@
 source .rvmrc
 
 # install bundler if necessary
-set -e
-
-gem install bundler --no-ri --no-rdoc && bundle install
+gem list --local bundler | grep bundler || gem install bundler || exit 1
 
 # debugging info
 echo USER=$USER && ruby --version && which ruby && which bundle
 
-bundle exec rspec spec
+rm -f log/*
+
+# conditionally install project gems from Gemfile
+bundle check || bundle install || exit 1
+
+test -e config/database.yml || (test -e config/database.yml.example && cp config/database.yml.example config/database.yml)
+test -e config/database.yml || (test -e config/database.example.yml && cp config/database.example.yml config/database.yml)
+
+RAILS_ENV=development rake db:version > /dev/null || rake db:create
+RAILS_ENV=test rake db:version  > /dev/null || rake db:create
+
+RAILS_ENV=development rake db:migrate test:prepare
+
+bundle exec rake
